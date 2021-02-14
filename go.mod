ea3c2ce907ff8bf15d92a7aca359ae212056e8a1
Aseem Sethi <aseemsethi@yahoo.com>
2021-02-14 01:40:57 +0000

    added yml parser

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/aws/aws-sdk-go v1.37.10
 	github.com/sirupsen/logrus v1.7.0
+	gopkg.in/yaml.v2 v2.2.8
 )
--- src/globals/globals.go
+++ src/globals/globals.go
@@ -2,21 +2,51 @@ package globals
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
 	"os"
 )
 
 type TcGlobals struct {
-	Name string
-	Log  *logrus.Logger
-	Sess *session.Session
-	// 	GRegion string
-	// 	GArn    string
-	// 	GConf   aws.Config
+	Name    string
+	Log     *logrus.Logger
+	Sess    *session.Session
+	GRegion string
+	GArn    string
+	GConf   aws.Config
+}
+
+type TcConfig struct {
+	Target struct {
+		Region string `yaml:"region"`
+		Id     string `yaml:"id"`
+	} `yaml:"target"`
+	Database struct {
+		Username string `yaml:"user"`
+		Password string `yaml:"pass"`
+	} `yaml:"database"`
 }
 
 var Globals = TcGlobals{Name: "Test Globals"}
+var Config TcConfig
+
+func parseYaml(tcg *TcGlobals) {
+	f, err := os.Open("config.yml")
+	if err != nil {
+		fmt.Println("Yaml file error", err)
+	}
+	defer f.Close()
+
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&Config)
+	if err != nil {
+		fmt.Println("Yaml decode error", err)
+	}
+	fmt.Println("Config: ", Config)
+}
 
 func (tcg *TcGlobals) Initialize() bool {
 	sess, err := session.NewSessionWithOptions(session.Options{
@@ -30,10 +60,12 @@ func (tcg *TcGlobals) Initialize() bool {
 	}
 	tcg.Sess = sess
 
-	// 	tcg.GRegion = region
-	// 	tcg.GArn = fmt.Sprintf("arn:aws:iam::%v:role/KVAccess", account)
-	// 	tcg.GConf = aws.Config{Region: aws.String(tcg.GRegion)}
-	// 	tcg.GConf.Credentials = stscreds.NewCredentials(tcg.Sess, tcg.GArn, func(p *stscreds.AssumeRoleProvider) {})
+	parseYaml(tcg)
+
+	tcg.GRegion = Config.Target.Region
+	tcg.GArn = fmt.Sprintf("arn:aws:iam::%v:role/KVAccess", Config.Target.Id)
+	tcg.GConf = aws.Config{Region: aws.String(tcg.GRegion)}
+	tcg.GConf.Credentials = stscreds.NewCredentials(tcg.Sess, tcg.GArn, func(p *stscreds.AssumeRoleProvider) {})
 
 	tcg.Log = logrus.New()
 	file, err := os.OpenFile("logs/tctool.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
